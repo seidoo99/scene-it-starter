543f8f37bbeb581e617c3c0fcf6fcb586eadeee4
seid zerihun <seidoo99@gmial.com>
2020-03-08 22:59:42 -0400

    watch list edited

--- index.js
+++ index.js
@@ -1,55 +1,7 @@
-// function renderMovies(movies) {
-
-//     MoviesHtml = movies.map((movie) => {
-//         return `
-//         <div class="card" style="width: 14rem">
-//         <img class="card-img-top" src="${movie.Poster}" alt="Card image cap">
-//         <div class="card-body">
-//         <p class="card-title">${movie.Title}<span>${movie.Year}</span></p>
-//         <a href="#" class="btn btn-primary" onclick=add()>Add</a>
-//         </div>
-//         </div>
-//    `
-//     }).join("");
-
-//     return MoviesHtml
-
-// }
-// var content = document.getElementById('movieContainer')
-// htmlsearch = document.getElementById('search-form')
-// htmlsearch.addEventListener('submit', function(e) {
-//     e.preventDefault()
-//         // console.log(e)
-//     var searchString = e.target.elements[0].value.toUpperCase()
-//         // console.log(searchString)
-//     var returnSearch = search(searchString, movieData)
-//         // console.log(returnSearch)
-//     content.innerHTML = renderMovies(returnSearch);
-
-// })
-
-// function search(nameKey, movieData) {
-//     var newArray = [];
-//     for (var i = 0; i < movieData.length; i++) {
-//         var movieTitle = movieData[i].Title.toUpperCase()
-//         if (movieTitle.includes(nameKey)) {
-//             newArray.push(movieData[i])
-
-//         }
-//     }
-//     return newArray
-// }
-
-// function movies() {
-//     var content = document.getElementById('movieContainer')
-//     content.innerHTML = renderMovies(movieData);
-// }
-// movies()
-
 document.addEventListener('DOMContentLoaded', function() {
-    function renderMovies(movieArray) {
-        movieHTML = movieArray.map(function(currentMovie) {
-            return `
+        function renderMovies(movieArray) {
+            movieHTML = movieArray.map(function(currentMovie) {
+                return `
                 <div class="card" style="width: 14rem">
                 <img class="card-img-top" src="${currentMovie.Poster}" alt="Card image cap">
                 <div class="card-body">
@@ -58,50 +10,52 @@ document.addEventListener('DOMContentLoaded', function() {
                 </div>
                 </div>
            `
+            })
+            return movieHTML.join('');
+        }
+        // var moviesContainer = document.getElementById("movies-container");
+        // document.getElementById("search-form").addEventListener("submit", function(e) {
+        //         e.preventDefault();
+        //         moviesContainer.innerHTML = renderMovies(movieData);
+        //     })
+
+        //search by name function
+        var content = document.getElementById('movieContainer')
+        document.getElementById('search-form').addEventListener('submit', function(e) {
+            e.preventDefault();
+            var searchString = e.target.elements[0].value.toUpperCase()
+                // console.log(searchString)
+            var returnSearch = search(searchString, movieData)
+                // console.log(returnSearch)
+            content.innerHTML = renderMovies(returnSearch);
+
         })
-        return movieHTML.join('');
-    }
-    // var moviesContainer = document.getElementById("movies-container");
-    // document.getElementById("search-form").addEventListener("submit", function(e) {
-    //         e.preventDefault();
-    //         moviesContainer.innerHTML = renderMovies(movieData);
-    //     })
-    var content = document.getElementById('movieContainer')
-    document.getElementById('search-form').addEventListener('submit', function(e) {
-        e.preventDefault();
-        var searchString = e.target.elements[0].value.toUpperCase()
-            // console.log(searchString)
-        var returnSearch = search(searchString, movieData)
-            // console.log(returnSearch)
-        content.innerHTML = renderMovies(returnSearch);
+
+        function search(nameKey, movieData) {
+            var newArray = [];
+            for (var i = 0; i < movieData.length; i++) {
+                var movieTitle = movieData[i].Title.toUpperCase()
+                if (movieTitle.includes(nameKey)) {
+                    newArray.push(movieData[i])
+
+                }
+            }
+            return newArray
+        }
 
     })
-
-    function search(nameKey, movieData) {
-        var newArray = [];
-        for (var i = 0; i < movieData.length; i++) {
-            var movieTitle = movieData[i].Title.toUpperCase()
-            if (movieTitle.includes(nameKey)) {
-                newArray.push(movieData[i])
-
-            }
-        }
-        return newArray
-    }
-
-})
-
+    // store to local storage temporarly aand save to watch list page
 function saveToWatchlist(imdbID) {
 
     var movie = movieData.find(function(currentMovie) {
         return currentMovie.imdbID == imdbID;
 
     });
-    saveToWatchlist(imdbID)
+    // saveToWatchlist(imdbID)
     var watchlistJSON = localStorage.getItem('watchlist');
     // console.log(watchlistJSON)
     var watchlist = JSON.parse(watchlistJSON);
-    if (watchLIst == null) {
+    if (watchlist == null) {
         watchlist = [];
     }
     watchlist.push(movie)
